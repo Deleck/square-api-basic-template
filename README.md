b8669f7bbd0a372abdcc221c65be280c44cfca12
Hector Ruiz <2017313052@uteq.edu.mx>
2021-03-23 14:34:56 -0600

    Basic Template using Square API

---
+++ README.md
@@ -0,0 +1,28 @@
+# Square API Basic Template
+
+Following the next steps for a correct use of the Square API
+
+## Pre-requirements:
+
+* Install any IDE (We recommend Visual Studio Code) for work.
+ 
+* Ensure you have NodeJS installed. You can download NodeJS here: https://nodejs.org/en
+
+* Ensure you have npm (a NodeJS package manager) installed. Type `npm -v` in your terminal, if not work please follow the instructions for your OS: https://www.npmjs.com/get-npm
+
+* Get your credentials from the [Square Developer Dashboard](https://developer.squareup.com/apps):
+
+## Steps:
+
+* Clone this project.
+
+* Open Visual Studio Code and a Terminal. In your terminal and type the following to install the packages:
+```
+npm install
+```
+This command will download the node_modules necessaries from the app.
+
+* Then to run the server:
+```
+npm start
+```
---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { Client, Environment, ApiError } = require('square');
+
+const app = express();
+const port = 3000;
+
+// Set the Access Token which is used to authorize to a merchant
+const accessToken = 'ACCESS_TOKEN';
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(__dirname));
+
+// Initialized the Square api client:
+//   Set sandbox environment for testing purpose
+//   Set access token
+const client = new Client({
+  environment: Environment.Sandbox,
+  accessToken: accessToken,
+});
+
+app.post('/process-payment', async (req, res) => {
+  const requestParams = req.body;
+
+  // Charge the customer's card
+  const paymentsApi = client.paymentsApi;
+  const requestBody = {
+    sourceId: requestParams.nonce,
+    amountMoney: {
+      amount: 100,
+      currency: 'USD'
+    },
+    locationId: requestParams.location_id,
+    idempotencyKey: requestParams.idempotency_key,
+  };
+
+  try {
+    const response = await paymentsApi.createPayment(requestBody);
+    res.status(200).json({
+      'title': 'Payment Successful',
+      'result': response.result
+    });
+  } catch(error) {
+    let errorResult = null;
+    if (error instanceof ApiError) {
+      errorResult = error.errors;
+    } else {
+      errorResult = error;
+    }
+    res.status(500).json({
+      'title': 'Payment Failure',
+      'result': errorResult
+    });
+  }
+});
+
+app.listen(
+  port,
+  () => console.log(`listening on - http://localhost:${port}`)
+);
